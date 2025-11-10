df79519daf2c5b2c4bcdc8a3569d5dfadd3da56c
Masoud <mmanson519@gmail.com>
2025-11-10 22:58:09 +0330

    usesearch not deleted

--- src/app/(a)/payment/success/page.js
+++ src/app/(a)/payment/success/page.js
@@ -1,7 +1,9 @@
 "use client";
 
+import { useSearchParams } from "next/navigation";
 
 export default function SuccessPage() {
+    const params = useSearchParams();
     const refId = params.get("ref_id");
 
     return (
