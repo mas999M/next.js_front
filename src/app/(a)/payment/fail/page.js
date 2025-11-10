49f1c45907807029aa5cafd442c73ce4414d8f50
Masoud <mmanson519@gmail.com>
2025-11-10 22:55:10 +0330

    usesearchparams deleted

--- src/app/(a)/payment/fail/page.js
+++ src/app/(a)/payment/fail/page.js
@@ -1,9 +1,7 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
 
 export default function FailPage() {
-    const params = useSearchParams();
     const message = params.get("message");
 
     return (
