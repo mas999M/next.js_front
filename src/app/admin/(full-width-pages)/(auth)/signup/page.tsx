2c3fec202f5bda2e66b439fc6e1274f477f3e209
Masoud <mmanson519@gmail.com>
2025-11-10 22:20:25 +0330

    final 1

--- src/app/admin/(full-width-pages)/(auth)/signup/page.tsx
+++ src/app/admin/(full-width-pages)/(auth)/signup/page.tsx
@@ -1,5 +1,5 @@
-import SignUpForm from "@/components/auth/SignUpForm";
 import { Metadata } from "next";
+import SignUpForm from "@/app/components/auth/SignUpForm";
 
 export const metadata: Metadata = {
   title: "Next.js SignUp Page | TailAdmin - Next.js Dashboard Template",
